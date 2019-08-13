5eae78088410a63cf7aa71bb371936ddd3484c65
Hiron Roy <hiron@atowndata.com>
2019-08-12 20:40:44 -0400

    MERGE + setup increment for pypi release of more recent.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-16") as fh:
 
 setuptools.setup(
     name="autoviml",
-    version="0.0.1",
+    version="0.0.2",
     author="Ram Seshadri",
     # author_email="author@example.com",
     description="Automatically Build Variant Interpretable ML models fast!",
